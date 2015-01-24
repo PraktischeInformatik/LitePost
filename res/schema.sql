70dfb6a4b9ad533daa6ab2cad9f5fdf6304582c2
Felix Breitweiser <felix.breitweiser@gmail.com>
2015-01-24 13:13:35 +0100

    added basic rounting and setup

---
+++ res/schema.sql
@@ -0,0 +1,94 @@
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users
+(
+    user_id INT PRIMARY KEY NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(102) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    admin INT(1) NOT NULL
+);
+
+DROP TABLE IF EXISTS Messages;
+CREATE TABLE Messages
+(
+    message_id INT PRIMARY KEY NOT NULL,
+    date DATE NOT NULL,
+    sender INT NOT NULL,
+    receiver INT NOT NULL,
+    subject TEXT NOT NULL,
+    content TEXT NOT NULL,
+    hidden INT(1) NOT NULL DEFAULT 0,
+    read INT(1) NOT NULL DEFAULT 0,
+    FOREIGN KEY(sender) REFERENCES users(user_id),
+    FOREIGN KEY(receiver) REFERENCES users(user_id)
+);
+
+DROP TABLE IF EXISTS Posts;
+CREATE TABLE Posts
+(
+    post_id INT PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    date DATE NOT NULL,
+    contact TEXT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+DROP TABLE IF EXISTS Events;
+CREATE TABLE Events 
+(
+  event_id INT PRIMARY KEY NOT NULL,
+  post_id INT NOT NULL,
+  date DATE NOT NULL,
+  FOREIGN KEY(post_id) REFERENCES Posts(post_id)
+);
+
+DROP TABLE IF EXISTS Comments;
+CREATE TABLE Comments
+(
+  comment_id INT PRIMARY KEY NOT NULL,
+  user_id INT NOT NULL,
+  content TEXT NOT NULL,
+  date DATE NOT NULL,
+  parent_id INT NOT NULL DEFAULT 0,
+  post_id INT NOT NULL,
+  FOREIGN KEY(post_id) REFERENCES Posts(post_id),
+  FOREIGN KEY(user_id) REFERENCES Users(user_id)
+);
+
+DROP TABLE IF EXISTS Sessions;
+CREATE TABLE Sessions
+(
+  token VARCHAR(255) NOT NULL,
+  expiration INT NOT NULL,
+  key VARCHAR(64) NOT NULL,
+  value TEXT NOT NULL,
+  PRIMARY KEY(token, key)
+);
+
+DROP TABLE IF EXISTS Ids;
+CREATE TABLE Ids
+(
+  table_name varchar(128) PRIMARY KEY NOT NULL,
+  next_id INT NOT NULL DEFAULT 0
+);
+
+DROP TABLE IF EXISTS Images;
+CREATE TABLE Images
+(
+  image_id INT PRIMARY KEY NOT NULL,
+  source TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS Post_has_Images;
+CREATE TABLE Post_has_Images 
+(
+  post_id INT NOT NULL,
+  image_id INT NOT NULL,
+  PRIMARY KEY (post_id, image_id),
+  FOREIGN KEY(post_id) REFERENCES Posts(post_id),
+  FOREIGN KEY(image_id) REFERENCES Images(image_id)
+);
--- src/org/pi/LitePost/App.java
+++ src/org/pi/LitePost/App.java
@@ -1,29 +1,67 @@
 package org.pi.LitePost;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.velocity.app.Velocity;
+import org.pi.LitePost.Router.Route;
+import org.pi.LitePost.controllers.FileController;
 
 import fi.iki.elonen.NanoHTTPD;
 
 public class App extends NanoHTTPD{
 
+	public static Properties config;
+
 	public App(int port) {
 		super(port);
+		
+		Router.add("files", Method.GET, "/public/.*", FileController::getFile);
 	}
 	
 	@Override public Response serve(IHTTPSession session) {
-		String html = "<html>"
-				+ "<head>"
-				+ "<title>Hello</title>"
-				+ "</head>"
-				+ "<body>"
-				+ "<h2>Hello, World!</h2>"
-				+ "</body>"
-				+ "</html>";
-		return new Response(html);
+		System.out.println(String.format("%s %s", session.getMethod(), session.getUri()));
+		Route route = Router.getHandler(session);
+		if (route != null) {
+			HashMap<String, String> args = Router.getRouteParams(session.getUri(), route);
+			return route.getHandler().handle(session, args, new HashMap<>());
+		}else {
+			return new Response(Response.Status.NOT_FOUND, "text/plain", "404 - Resource not found");
+		}
 	}
 
 	public static void main(String[] args) {
-		App app = new App(8080);
+		Velocity.init();
+		
+		InputStream inStream = null;
+		try {
+			inStream = new FileInputStream("res" + File.separatorChar + "config.properties");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		config = new Properties();
+		try {
+			config.load(inStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		String serverport = (String) config.getOrDefault("Serverport", "8080");
+		int port = 8080;
+		try {
+			port = Integer.parseInt(serverport);
+		}catch(NumberFormatException e) {
+			System.out.println("Error reading serverport from configuration.");
+			System.out.println("Falling back to default port: " + port);
+		}
+		
+		App app = new App(port);
 		try {
 			app.start();
 		} catch (IOException e) {
@@ -31,7 +69,7 @@ public class App extends NanoHTTPD{
 			System.exit(-1);
 		}
 		
-		System.out.println("Server startet. Hit Enter to stop.");
+		System.out.println(String.format("Server startet on port %d. Hit Enter to stop.", port));
 		
 		try {
 			System.in.read();
