322e59c24f12bb4403002a2bcaeedc8d1b748a1d
Julia <julia-christin.moos@gmx.net>
2015-01-26 15:50:56 +0100

    added two Exceptions and a package for them

---
+++ src/org/pi/litepost/exceptions/UseranameExistsException.java
@@ -0,0 +1,36 @@
+package org.pi.litepost.exceptions;
+
+public class UseranameExistsException extends Exception {
+
+	/**
+	 * Exception is thrown when username already exists in database
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UseranameExistsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UseranameExistsException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UseranameExistsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UseranameExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UseranameExistsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
