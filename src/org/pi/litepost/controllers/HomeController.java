410527323ac1b962e1987bc2c9b0aa69546e57f6
Felix Breitweiser <felix.breitweiser@gmail.com>
2015-01-29 10:01:50 +0100

    added error/debug.vm

--- src/org/pi/litepost/controllers/HomeController.java
+++ src/org/pi/litepost/controllers/HomeController.java
@@ -3,14 +3,26 @@ package org.pi.litepost.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.pi.litepost.Router;
 import org.pi.litepost.View;
 import org.pi.litepost.applicationLogic.Model;
+import org.pi.litepost.databaseAccess.DatabaseCriticalErrorException;
 
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Response;
 
 public class HomeController {
 	public static Response getHome(IHTTPSession session, Map<String, String> routeArgs, HashMap<String, Object> data, Model model) {
+		try {
+			model.getPostManager().insert("test", "testing", "1234", 0);
+		} catch (DatabaseCriticalErrorException e) {
+			return Router.error(e);
+		}
+		try {
+			data.put("posts", model.getPostManager().getAll());
+		} catch (Exception e) {
+			return Router.error(e);
+		}
 		return new Response(View.make("home", data));
 	}
 	
