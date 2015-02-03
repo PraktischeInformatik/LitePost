7fec36b3ef53dcd57f920609b242c01d4a42828a
Felix Breitweiser <felix.breitweiser@gmail.com>
2015-02-03 11:24:52 +0100

    added Resource management for scripts and styles

--- src/org/pi/litepost/View.java
+++ src/org/pi/litepost/View.java
@@ -3,6 +3,7 @@ package org.pi.litepost;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.velocity.Template;
@@ -35,6 +36,7 @@ public class View {
 			context.put(key, data.get(key));
 		}
 		context.put("Router", Router.class);
+		context.put("Resources", new Resources());
 		StringWriter writer = new StringWriter();
 		 getTemplate(template).merge(context, writer);
 		return writer.toString();
@@ -62,6 +64,7 @@ public class View {
 		          }
 		        });
 	}
+	
 	public static String sanitizePostContent(String string) {
 		StringBuilder sb = new StringBuilder();
 		HtmlSanitizer.sanitize(string, POST_CONTENT_POLICY.apply(getRenderer(sb)));
@@ -72,5 +75,47 @@ public class View {
 		StringBuilder sb = new StringBuilder();
 		HtmlSanitizer.sanitize(string, STRICT_POLICY.apply(getRenderer(sb)));
 		return sb.toString();
+	}
+	
+	private static class Resources {
+		private ArrayList<String> scriptsHeader = new ArrayList<>();
+		private ArrayList<String> scriptsFooter = new ArrayList<>();
+		private ArrayList<String> styles = new ArrayList<>();
+		
+		private String styleTemplate = "<link rel=\"stylesheet\" href=\"%s\">";
+		private String scriptTemplate = "<script type=\"text/javascript\" src=\"%s\">";
+		
+		public Resources() {
+			addStyle("/public/css/style.css");
+		}
+		
+		public void addStyle(String uri) {	
+			styles.add(String.format(styleTemplate, uri));
+		}
+		
+		@SuppressWarnings("unused")
+		public void addScriptHeader(String uri) {	
+			scriptsHeader.add(String.format(scriptTemplate, uri));
+		}
+		
+		@SuppressWarnings("unused")
+		public void addScriptFooter(String uri) {	
+			scriptsFooter.add(String.format(scriptTemplate, uri));
+		}
+		
+		@SuppressWarnings("unused")
+		public ArrayList<String> getStyles() {
+			return styles;
+		}
+		
+		@SuppressWarnings("unused")
+		public ArrayList<String> getScriptsHeader() {
+			return scriptsHeader;
+		}
+		
+		@SuppressWarnings("unused")
+		public ArrayList<String> getScriptsFooter() {
+			return scriptsFooter;
+		}
 	}
 }
