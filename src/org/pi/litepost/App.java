268ed80619059905a2e24ac856900d2e7ac3f1ef
Felix Breitweiser <Felix@Breitweiser.de>
2015-02-07 11:55:43 +0100

    added route for comments

--- src/org/pi/litepost/App.java
+++ src/org/pi/litepost/App.java
@@ -42,7 +42,8 @@ public class App extends NanoHTTPD{
 		Router.add("allPosts", Method.GET, "/posts", PostController::getAll);
 		Router.add("newPost", Method.GET, "/posts/new", PostController::getNew);
 		Router.add("insertPost", Method.POST, "/posts/new", PostController::postNew);
-		Router.add("singlePost", Method.GET, "/posts/{post_id}", PostController::getSingle);
+		Router.add("singlePost", Method.GET, "/post/{post_id}", PostController::getSingle);
+		Router.add("commentPost", Method.POST, "/comments/new", PostController::commentPost);
 		
 	}
 	
