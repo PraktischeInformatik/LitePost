8abc3bae874527c1cb0d455f7e6f8c1b10880067
Felix Breitweiser <Felix@Breitweiser.de>
2015-02-07 11:00:34 +0100

    moved js to separate file

---
+++ public/js/newpost.js
@@ -0,0 +1,22 @@
+var picker = new Pikaday({
+  field: document.getElementById("input-date")
+});
+
+function toggle_event(checkbox) {
+  var input = document.getElementById("date-div");
+  if(checkbox.checked) {
+    input.className = "show"
+  }else {
+    input.className = ""
+  }
+}
+
+function serialize_text() {
+  var content = document.getElementById("input-content");
+  content.value = editor.serialize()["input-editor"].value;
+}
+
+var editor = new MediumEditor('#input-editor', {
+   placeholder: 'Inhalt',
+   buttons: ['bold', 'italic', 'underline', 'anchor', 'header2']
+});
